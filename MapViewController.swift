e0f6637e188e54f50eb528ce7985d208713811be
BAZZAOUI chaymae <bazzaouichaymae@MacBook-Pro-de-BAZZAOUI.local>
2016-10-11 12:55:37 +0100

    finished chapter 6

---
+++ MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  WoldTrotter
+//
+//  Created by BAZZAOUI chaymae on 10/8/16.
+//  Copyright © 2016 Klechkowski. All rights reserved.
+//
+
+import Foundation
---
+++ WoldTrotter/ConversionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewController.swift
+//  WoldTrotter
+//
+//  Created by BAZZAOUI chaymae on 10/5/16.
+//  Copyright © 2016 Klechkowski. All rights reserved.
+//
+
+import Foundation
